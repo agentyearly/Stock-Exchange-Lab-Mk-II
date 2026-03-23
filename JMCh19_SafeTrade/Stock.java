ef2af2aeaefaa144a8bc5ab26391e66a0961c5ce
RaymondMao0 <raymondmao2009@gmail.com>
2026-03-23 09:48:13 -0700

    Update Stock.java

--- JMCh19_SafeTrade/Stock.java
+++ JMCh19_SafeTrade/Stock.java
@@ -1,57 +1,171 @@
-// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.lang.reflect.*;
+import java.text.DecimalFormat;
 
-public class StockExchange {
-   private Map<String, Stock> listedStocks = new HashMap();
+/**
+ * Represents a stock in the SafeTrade project
+ */
+public class Stock {
+    public static DecimalFormat money = new DecimalFormat("0.00");
 
-   public StockExchange() {
-   }
+    private String stockSymbol;
+    private String companyName;
+    private double loPrice, hiPrice, lastPrice;
+    private int volume;
+    private PriorityQueue<TradeOrder> buyOrders, sellOrders;
 
-   public String getQuote(String var1) {
-      if (this.listedStocks.containsKey(var1)) {
-         Stock var2 = (Stock)this.listedStocks.get(var1);
-         return var2.getQuote();
-      } else {
-         return var1 + " not found";
-      }
-   }
 
-   public void listStock(String var1, String var2, double var3) {
-      Stock var5 = new Stock(var1, var2, var3);
-      this.listedStocks.put(var1, var5);
-   }
+    public Stock(java.lang.String symbol, java.lang.String name, double price) {
+        stockSymbol = symbol;
+        companyName = name;
+        loPrice = price;
+        hiPrice = price;
+        lastPrice = price;
+        volume = 0;
+        sellOrders = new PriorityQueue<TradeOrder>(10, new PriceComparator(true));
+        buyOrders = new PriorityQueue<TradeOrder>(10, new PriceComparator(false));
+    }
 
-   public void placeOrder(TradeOrder var1) {
-      if (this.listedStocks.containsKey(var1.getSymbol())) {
-         Stock var2 = (Stock)this.listedStocks.get(var1.getSymbol());
-         var2.placeOrder(var1);
-      } else {
-         var1.getTrader().receiveMessage(var1.getSymbol() + " not found");
-      }
+    //
+    // The following are for test purposes only
+    //
 
-   }
+    protected String getStockSymbol() {
+        return stockSymbol;
+    }
 
-   protected Map<String, Stock> getListedStocks() {
-      return this.listedStocks;
-   }
+    protected String getCompanyName() {
+        return companyName;
+    }
 
-   public String toString() {
-      String var1 = this.getClass().getName() + "[";
-      String var2 = "";
-      Field[] var3 = this.getClass().getDeclaredFields();
+    protected double getLoPrice() {
+        return loPrice;
+    }
 
-      for(Field var7 : var3) {
-         try {
-            var1 = var1 + var2 + var7.getType().getName() + " " + var7.getName() + ":" + String.valueOf(var7.get(this));
-         } catch (IllegalAccessException var9) {
-            System.out.println(var9);
-         }
+    protected double getHiPrice() {
+        return hiPrice;
+    }
 
-         var2 = ", ";
-      }
+    protected double getLastPrice() {
+        return lastPrice;
+    }
 
-      return var1 + "]";
-   }
+    protected int getVolume() {
+        return volume;
+    }
+
+    protected PriorityQueue<TradeOrder> getBuyOrders() {
+        return buyOrders;
+    }
+
+    protected PriorityQueue<TradeOrder> getSellOrders() {
+        return sellOrders;
+    }
+
+    protected void executeOrders() {
+    while (!buyOrders.isEmpty() && !sellOrders.isEmpty()) {
+        TradeOrder buy = buyOrders.peek();
+        TradeOrder sell = sellOrders.peek();
+
+        double price;
+
+        if (buy.isLimit() && sell.isLimit()) {
+            if (buy.getPrice() < sell.getPrice()) {
+                return;
+            }
+            price = sell.getPrice();
+        } else if (buy.isLimit() ^ sell.isLimit()) {
+            price = buy.isLimit() ? buy.getPrice() : sell.getPrice();
+        } else { 
+            price = lastPrice;
+        }
+
+        int shares = Math.min(buy.getShares(), sell.getShares());
+        buy.subtractShares(shares);
+        sell.subtractShares(shares);
+
+        if (buy.getShares() == 0)  {
+            buyOrders.remove();
+        }
+        if (sell.getShares() == 0) {
+            sellOrders.remove();
+        }
+        if (price < loPrice)  {
+            loPrice = price;
+        }
+        if (price > hiPrice) {
+            hiPrice = price;
+        }
+        volume += shares;
+
+        Trader bt = buy.getTrader();
+        Trader st = sell.getTrader();
+
+        bt.receiveMessage("You bought: " + shares + " " + buy.getSymbol() + " at " +
+                money.format(price) + " amt " + money.format(shares * price));
+        st.receiveMessage("You sold: " + shares + " " + sell.getSymbol() + " at " +
+                money.format(price) + " amt " + money.format(shares * price));
+    }
+}
+
+
+    public String getQuote() {
+        String first = companyName + " (" + stockSymbol + ")" + "\n" + "Price: " + lastPrice + " hi: " + hiPrice + " lo: " + money.format(loPrice) + " vol: " + volume + "\n";
+        String ask = "";
+        if (!sellOrders.isEmpty()) {
+            TradeOrder sellOrder = sellOrders.peek();
+            ask = " Ask: " + sellOrder.getPrice() + " size: " + sellOrder.getShares();
+        } else {
+            ask = " Ask: none";
+        }
+            String bid = "";
+           if (!buyOrders.isEmpty()) {
+                TradeOrder buyOrder = buyOrders.peek();
+                bid = " Bid: " + buyOrder.getPrice() + " size: " + buyOrder.getPrice();
+            } else {
+                bid = " Bid: none";
+            }
+        return first + ask + bid;
+    }
+
+    public void placeOrder(TradeOrder order) {
+        String msg = "New Order: ";
+        if (order.isBuy()) {
+            buyOrders.add(order);
+            msg.concat("buy ");
+        } else {
+            sellOrders.add(order);
+            msg.concat("Sell ");
+        }
+        msg.concat(order.getSymbol() + " (" + order.getTrader().getName() + ")" + "\n" + order.getShares()+ " shares at $" + order);
+    }
+
+    /**
+     * <p>
+     * A generic toString implementation that uses reflection to print names and
+     * values of all fields <em>declared in this class</em>. Note that
+     * superclass fields are left out of this implementation.
+     * </p>
+     * 
+     * @return a string representation of this Stock.
+     */
+    public String toString() {
+        String str = this.getClass().getName() + "[";
+        String separator = "";
+
+        Field[] fields = this.getClass().getDeclaredFields();
+
+        for (Field field : fields) {
+            try {
+                str += separator + field.getType().getName() + " "
+                        + field.getName() + ":" + field.get(this);
+            } catch (IllegalAccessException ex) {
+                System.out.println(ex);
+            }
+
+            separator = ", ";
+        }
+
+        return str + "]";
+    }
 }
