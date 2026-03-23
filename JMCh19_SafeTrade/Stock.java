5a67ba53f85905b9831cae1e7b20f170e79e0a62
RaymondMao0 <raymondmao2009@gmail.com>
2026-03-23 09:44:36 -0700

    Update Stock.java

--- JMCh19_SafeTrade/Stock.java
+++ JMCh19_SafeTrade/Stock.java
@@ -1,98 +1,57 @@
-import java.util.*;
-import java.lang.reflect.*;
-import java.text.DecimalFormat;
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * Represents a stock in the SafeTrade project
- */
-public class Stock
-{
-    public static DecimalFormat money = new DecimalFormat( "0.00" );
+public class StockExchange {
+   private Map<String, Stock> listedStocks = new HashMap();
 
-    private String stockSymbol;
-    private String companyName;
-    private double loPrice, hiPrice, lastPrice;
-    private int volume;
-    private PriorityQueue<TradeOrder> buyOrders, sellOrders;
+   public StockExchange() {
+   }
 
-    // TODO complete class
+   public String getQuote(String var1) {
+      if (this.listedStocks.containsKey(var1)) {
+         Stock var2 = (Stock)this.listedStocks.get(var1);
+         return var2.getQuote();
+      } else {
+         return var1 + " not found";
+      }
+   }
 
-    
-    //
-    // The following are for test purposes only
-    //
-    
-    protected String getStockSymbol()
-    {
-        return stockSymbol;
-    }
-    
-    protected String getCompanyName()
-    {
-        return companyName;
-    }
-    
-    protected double getLoPrice()
-    {
-        return loPrice;
-    }
-    
-    protected double getHiPrice()
-    {
-        return hiPrice;
-    }
+   public void listStock(String var1, String var2, double var3) {
+      Stock var5 = new Stock(var1, var2, var3);
+      this.listedStocks.put(var1, var5);
+   }
 
-    protected double getLastPrice()
-    {
-        return lastPrice;
-    }
-    
-    protected int getVolume()
-    {
-        return volume;
-    }
+   public void placeOrder(TradeOrder var1) {
+      if (this.listedStocks.containsKey(var1.getSymbol())) {
+         Stock var2 = (Stock)this.listedStocks.get(var1.getSymbol());
+         var2.placeOrder(var1);
+      } else {
+         var1.getTrader().receiveMessage(var1.getSymbol() + " not found");
+      }
 
-    protected PriorityQueue<TradeOrder> getBuyOrders()
-    {
-        return buyOrders;
-    }
-    
-    protected PriorityQueue<TradeOrder> getSellOrders()
-    {
-        return sellOrders;
-    }
-    
-    /**
-     * <p>
-     * A generic toString implementation that uses reflection to print names and
-     * values of all fields <em>declared in this class</em>. Note that
-     * superclass fields are left out of this implementation.
-     * </p>
-     * 
-     * @return a string representation of this Stock.
-     */
-    public String toString()
-    {
-        String str = this.getClass().getName() + "[";
-        String separator = "";
+   }
 
-        Field[] fields = this.getClass().getDeclaredFields();
+   protected Map<String, Stock> getListedStocks() {
+      return this.listedStocks;
+   }
 
-        for ( Field field : fields )
-        {
-            try
-            {
-                str += separator + field.getType().getName() + " "
-                    + field.getName() + ":" + field.get( this );
-            }
-            catch ( IllegalAccessException ex )
-            {
-                System.out.println( ex );
-            }
+   public String toString() {
+      String var1 = this.getClass().getName() + "[";
+      String var2 = "";
+      Field[] var3 = this.getClass().getDeclaredFields();
 
-            separator = ", ";
-        }
+      for(Field var7 : var3) {
+         try {
+            var1 = var1 + var2 + var7.getType().getName() + " " + var7.getName() + ":" + String.valueOf(var7.get(this));
+         } catch (IllegalAccessException var9) {
+            System.out.println(var9);
+         }
 
-        return str + "]";
-    }
+         var2 = ", ";
+      }
+
+      return var1 + "]";
+   }
 }
